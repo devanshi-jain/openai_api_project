56ac8902749f168e481eb08f363ec092b52ef5ef
Devanshi Jain <djain@ucsd.edu>
2023-04-28 11:52:49 -0700

    Added a readme file, create a requests and main

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+# Steps to get set up for this repository : 
+
+Install require libraries by running the following code:
+```
 pip3 install python-dotenv
 pip3 install openai
+```
---
+++ pdf_summarizer.py
@@ -0,0 +1,19 @@
+Authorization: Bearer OPENAI_API_KEY
+
+import openai
+import os
+import openai_requests
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+
+# Get value of OPENAI_API_KEY environment variable
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+# Set the API key for the OpenAI API client
+openai.api_key = openai_api_key
+
+# Use the OpenAI API client to make API requests
+# ...
