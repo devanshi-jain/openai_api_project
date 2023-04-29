22d728971598902e91d211e151f6f4f972e2c785
Devanshi Jain <djain@ucsd.edu>
2023-04-29 15:40:07 -0700

    fixed all code. it runs, let's goooooo

--- pdf_parser.py
+++ pdf_parser.py
@@ -23,37 +23,53 @@ def pdf_parse_text(file_path, page_num=None):
         interpreter = PDFPageInterpreter(resource_manager, device)
         title = ''
         author = ''
-        page_counter = 0
+        page_counter = -1
         if page_num is None:
             for page in PDFPage.get_pages(in_file, maxpages=0, caching=True, check_extractable=True):
                 page_counter += 1
                 interpreter.process_page(page)
                 content = output_string.getvalue()
+                text = f"""
+                {content}
+                """
+                prompt = f"""
+                Your task is to summarize pages of a book based on the content of the page provided.
+                The page text is delimited by triple backticks.
+                The summary is intended for someone trying to read the book on their own and need help understanding the content.
+                It should be a short paragraph of 3-5 sentences.
+                ```{content}```
+                
+                """
+                response = get_completion(prompt)
                 output_string.seek(0) #move cursor to beginning of file : 
                 # ensures that subsequent calls to output_string.getvalue() start from the beginning of the string buffer
                 output_string.truncate(0) #clear the string buffer so that it only contains the text parsed on the current page. 
-                print(f'Page {page_num[page_counter]}:\n{content}')
+                print(f'Page {page_num[page_counter]}:\n{response}')
         else:
             for page in PDFPage.get_pages(in_file, page_num, maxpages=0, caching=True, check_extractable=True):
+                page_counter += 1
                 interpreter.process_page(page)
                 content = output_string.getvalue()
                 output_string.seek(0)
                 output_string.truncate(0)
-                print(f'The summary of Page {page_num[page_counter]}:\n{content}')
+                # print(f'The summary of Page {page_num[page_counter]}:\n{content}')
+                text = f"""
+                {content}
+                """
+                prompt = f"""
+                Your task is to summarize pages of a book based on the content of the page provided.
+                The page text is delimited by triple backticks.
+                The summary is intended for someone trying to read the book on their own and need help understanding the content.
+                It should be a short paragraph of 4-6 sentences.
+                ```{content}```
+                """
+                response = get_completion(prompt)
+                print(f'Page {page_num[page_counter]}:\n{response}')
     device.close()
     content = output_string.getvalue()
     output_string.close()
     return content
 
-
-
-        # TODO : Write prompt and add text to print summary for each section
-        # Generate summaries for each section using OpenAI API
-        # for section in sections:
-        #     completion_text = get_completion(section)
-        #     summary = completion_text["choices"][0]["text"].strip()
-        #     print(summary)
-        
-pdf_text = pdf_parse_text('The Subtle Art.pdf', [72])
-# toc = extract_toc(pdf_text)
+    
+pdf_text = pdf_parse_text('The Subtle Art.pdf', [4,72])
 print(pdf_text)
