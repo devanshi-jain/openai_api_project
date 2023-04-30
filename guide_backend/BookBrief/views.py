95289f9ab68bc7cd8c84beaf009f350e9b25755e
Devanshi Jain <djain@ucsd.edu>
2023-04-30 00:21:32 -0700

    website published.

--- guide_backend/BookBrief/views.py
+++ guide_backend/BookBrief/views.py
@@ -4,21 +4,57 @@ from rest_framework.response import Response
 from .models import Book
 from .serializers import BookSerializer
 
-class BookListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+import io
+from django.shortcuts import render
+from django.http import JsonResponse
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class BookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+
+# from PyPDF2 import PdfFileReader
+# from .forms import PDFUploadForm
+
+# def parse_pdf(request):
+#     if request.method == 'POST':
+#         form = PDFUploadForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             # Get the uploaded PDF file and page numbers
+#             pdf_file = form.cleaned_data['pdf_file']
+#             page_numbers = form.cleaned_data['page_numbers']
+            
+#             # Parse the PDF file and extract text
+#             pdf_reader = PdfFileReader(io.BytesIO(pdf_file.read()))
+#             if page_numbers == 'all':
+#                 page_range = range(pdf_reader.getNumPages())
+#             else:
+#                 start_page, end_page = page_numbers.split('-')
+#                 page_range = range(int(start_page) - 1, int(end_page))
+#             text = ''
+#             for i in page_range:
+#                 text += pdf_reader.getPage(i).extractText()
+            
+#             # Return the extracted text as a JSON response
+#             return JsonResponse({'text': text})
+#     else:
+#         form = PDFUploadForm()
+    
+#     return render(request, 'index.html', {'form': form})
+
+
+# class BookListCreateAPIView(generics.ListCreateAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+
+#     def create(self, request, *args, **kwargs):
+#         serializer = self.serializer_class(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# class BookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
 
 # Views in Django are functions that take an HTTP request and return an HTTP response.
 # They define how the application handles various HTTP requests. 
